a1b207f356e3fe0c6e9a2dc678c56f546dc3969f
icodetree <icodetree@naver.com>
2022-09-19 18:21:41 +0900

    modify filter bug

--- study/javascript_todolist/src/components/ItemFilter.js
+++ study/javascript_todolist/src/components/ItemFilter.js
@@ -3,9 +3,9 @@ import Component from "../core/Component.js";
 export default class ItemFilter extends Component {
     template () {
         return `
-            <button class="filterBtn" data-is-filter="0"><span><ion-icon name="layers"></ion-icon></span>전체</button>
-            <button class="filterBtn" data-is-filter="1"><span><ion-icon name="radio-button-on"></ion-icon></span>완료</button>
-            <button class="filterBtn" data-is-filter="2"><span><ion-icon name="radio-button-off"></ion-icon></span>미완료</button>  
+            <span><ion-icon name="layers"></ion-icon><button class="filterBtn" data-is-filter="0">전체</button></span>
+            <span><ion-icon name="radio-button-on"></ion-icon><button class="filterBtn" data-is-filter="1">완료</button></span>
+            <span><ion-icon name="radio-button-off"></ion-icon><button class="filterBtn" data-is-filter="2">미완료</button></span>
         `;
     }
 
